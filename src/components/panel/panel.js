5dda346f8d5aa6343685078bb5a067153fe45b1b
Luan Antony <fplpgluan@gmail.com>
2024-08-13 16:36:48 -0300

    adicionado comentario da importancia da arrow function em react

--- src/components/panel/panel.js
+++ src/components/panel/panel.js
@@ -25,6 +25,8 @@ class Panel extends Component{
 }
 
 /* 
+Deve ser usado um arrow funcion sempre, pois o this pegaria a function como contexto e o tornaria undefined. Mas ainda pode ser executado caso o this seja passado para uma variavel e então passado como variavel.setState
+
 O state vai servir para ser alterado, algo que uma prop não podera fazer nesse caso especifico que é mudar o titulo do painel por outro ao clicar
 
 Quando o componente for renderizado, o h2 vai ser buscado pelo this.state.title que esta na função constructor (Titulo do painel). O codigo a cima mostra como sera mostrado o estado inicial do titulo
